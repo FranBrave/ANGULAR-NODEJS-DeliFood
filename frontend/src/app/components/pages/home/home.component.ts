ee3841f8af7b716f758560ca8b803b28d1cb8533
Fran <valiente.fco@gmail.com>
2023-02-22 15:23:56 +0100

    finish back and front connection

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Food } from 'src/app/shared/models/Food';
 import { FoodService } from '../../../services/food.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,13 +14,18 @@ export class HomeComponent {
   foods:Food[] = [];
 
   constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    let foodsObservable:Observable<Food[]>
     activatedRoute.params.subscribe((params) => {
       if(params.search)
-      this.foods = this.foodService.getAllFoodBySearch(params.search)
+      foodsObservable = this.foodService.getAllFoodBySearch(params.search)
       else if (params.tag)
-      this.foods = this.foodService.getAllFoodByTag(params.tag)
+      foodsObservable = this.foodService.getAllFoodByTag(params.tag)
       else
-      this.foods = foodService.getAll();
+      foodsObservable = foodService.getAll();
+
+      foodsObservable.subscribe((server) => {
+        this.foods = server;
+      })
     })
   }
 
