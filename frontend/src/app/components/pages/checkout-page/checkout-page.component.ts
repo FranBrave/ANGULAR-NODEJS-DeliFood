f663cb81cd74e85e0e170ac2c6526dd2785dfb29
Fran <valiente.fco@gmail.com>
2023-03-12 21:53:56 +0100

    starting with payment parcel

--- frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
+++ frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CartService } from 'src/app/services/cart.service';
 import { UserService } from 'src/app/services/user.service';
 import { Order } from 'src/app/shared/models/Order';
+import { OrderService } from '../../../servies/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout-page',
@@ -18,7 +20,9 @@ export class CheckoutPageComponent implements OnInit {
   constructor( cartService: CartService,
                 private formBuilder: FormBuilder,
                 private userService: UserService,
-                private toastrService: ToastrService ) {
+                private toastrService: ToastrService,
+                private orderService: OrderService,
+                private router: Router ) {
                   const cart = cartService.getCart();
                   this.order.items = cart.items;
                   this.order.totalPrice = cart.totalPrice;
@@ -43,11 +47,22 @@ createOrder() {
     return;
   };
 
+  if(!this.order.addressLatLng) {
+    this.toastrService.warning('Por favor, selecciona tu ubicación en el mapa', 'Ubicación');
+    return;
+  }
+
   this.order.name = this.fc.name.value;
   this.order.address = this.fc.address.value;
 
-  console.log(this.order);
-
+  this.orderService.create(this.order).subscribe({
+    next: () => {
+        this.router.navigateByUrl('/payment');
+  },
+  error: (errorRespone) => {
+    this.toastrService.error(errorRespone.error, 'Cart')
+  }
+})
 }
 
 }
